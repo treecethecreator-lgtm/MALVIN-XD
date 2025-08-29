e62a8fa1e10ba6796ae1636d5fe03437b9d69264
treecethecreator-lgtm <treecethecreator@gmail.com>
2025-08-30 00:06:38 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~13cjUQ6Y#0lcn7w2nOmo77yvJ5CLawp2DHEtcOpnIHuPW4Y-qEjA",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
